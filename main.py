0692b7b5c8f577d9b4e1f0f1b41809ef73c67f2d
Gui Ribeiro <guilhermemaiaribeiro@gmail.com>
2018-07-03 20:54:03 +0100

    small bug fixing and return as a dictionary

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def get_price_vat(product_id):
         if n['product_id'] == product_id:
             price = n['price']
             vat = n['vat_band']
-
-            return price, vat
+            details = {"price": price, "vat":vat}
+            return details
 
 
 def get_vat_bands(bands):
@@ -27,16 +27,16 @@ def get_vat_bands(bands):
 
 class Order(tornado.web.RequestHandler):
     def post(self):
-        prices = data = json.loads(self.request.body)
+        prices = json.loads(self.request.body)
         total_value = 0
         total_vat = 0
         for item in prices['order']['items']:
             product_id = item['product_id']
             quantity = item['quantity']
             details = get_price_vat(product_id)
-            applicable_vat = get_vat_bands(details[1])
-            total_value += quantity * details[0]
-            total_vat += (quantity * details[0]) * applicable_vat
+            applicable_vat = get_vat_bands(details['vat'])
+            total_value += quantity * details['price']
+            total_vat += (quantity * details['price']) * applicable_vat
 
         self.finish({"order": total_value, "VAT": round(total_vat, 3), "Total": total_value+total_vat})
 
